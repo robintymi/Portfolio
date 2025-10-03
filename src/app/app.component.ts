1a4677705e30338d6db6bcd5b308143bb90154af
Robin Erike <robinerike@gmail.com>
2025-10-03 13:13:13 +0200

    firstpush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
