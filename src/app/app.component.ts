ee7adcba7d599f20f3a009504f80a0eb5364d995
Robin Erike <robinerike@gmail.com>
2025-10-06 12:56:05 +0200

    why-me section first html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { WhyMeComponent } from './why-me/why-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroSectionComponent, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, HeroSectionComponent, NavbarComponent, WhyMeComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
