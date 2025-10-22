304e7766935b600e646b18913f837dac45be14da
Robin Erike <robinerike@gmail.com>
2025-10-22 12:18:00 +0200

    sticky navbar, DE-EN- active and hovers

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  currentLang: 'de' | 'en' = 'en';
+
+  switchLang(lang: 'de' | 'en') {
+    this.currentLang = lang;
+  }
 
 }
