742a1858d018fe829194cb8fe67b49589e318cb3
Robin Erike <robinerike@gmail.com>
2025-11-30 15:22:08 +0100

    lets talk button mobile skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { ScrollService } from '../shared/scroll.service';
 
 @Component({
   selector: 'app-skills',
@@ -9,5 +11,13 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  constructor(
+    private router: Router,
+    private scrollService: ScrollService
+  ) { }
 
+  async navigateToContact() {
+    await this.router.navigate(['/'], { fragment: 'contact' });
+    setTimeout(() => this.scrollService.scrollToSection('contact'), 0);
+  }
 }
